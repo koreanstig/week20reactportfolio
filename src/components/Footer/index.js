cd8d2a53fcddc96a38789f8cc5c7ac6ecc9067bb
Jung David Lee <jungdavidlee@gmail.com>
2021-05-13 19:32:56 -0700

    created header/footer, put content into header

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <div className="footer" id="footer">
+        <a href="https://github.com/koreanstig"><img src="https://img.icons8.com/fluent/48/000000/github.png" alt="Github" className="icon"/></a>
+        <a href="https://www.linkedin.com/in/jung-lee1991/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+    </div>
+    )
+}
+
+export default Footer;
