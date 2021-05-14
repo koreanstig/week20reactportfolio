cf8e1a1cea03ce66294715ce8aed07cdc5f25843
Jung David Lee <jungdavidlee@gmail.com>
2021-05-13 19:58:41 -0700

    added resume

---
+++ src/components/Resume/index.js
@@ -0,0 +1,30 @@
+import React from 'react'; 
+
+function Resume() {
+
+return(
+<section class="mb-5">
+    <h2 className="resume">Resume</h2>
+        <hr></hr>
+       
+           
+
+        <div class="row justify-content-center" id="resume">
+            <div class="mt-5 pl-5 pr-5">
+                
+                <p class="mt-5">
+                <a href="https://www.linkedin.com/in/jung-lee1991/">
+                    <img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="inkedIn"/></a>
+                </p>
+               
+               <a href="https://koreanstig.github.io/portfolio/resume.html" class="link">Resume</a>
+               
+            </div>
+        </div>
+
+        
+</section>
+);
+}
+
+export default Resume;
