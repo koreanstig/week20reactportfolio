71d6a7da330809678e394cd6d6d71b01f87b88df
Jung David Lee <jungdavidlee@gmail.com>
2021-05-13 19:21:31 -0700

    created about section

---
+++ src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About() {
+    return(
+    <section className="title">
+      <h2 class="name">Jung (David) Lee</h2>
+      <hr></hr>
+      
+      <div className="row justify-content-center">
+        <div className="col-10" id="about-section">
+            <img class="mb-5" id="avatar" src="https://avatars.githubusercontent.com/u/69485203?v=4" alt="Jung Lee"/>
+        
+            <p>Currently a student in the University of Washington Full Stack Web Develoment Bootcamp. Specializing in both Front End and Back End technologies.</p>
+        </div>  
+      </div>
+    </section> 
+)}
+  
+export default About;
